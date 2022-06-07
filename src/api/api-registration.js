a7fc2b4a48ec63cb857766dd32fa519c4015c4e1
grailwilly <grailjwi11y@gmail.com>
2022-06-07 18:40:11 +0800

    Add registration api, component, and success create account

--- src/api/api-registration.js
+++ src/api/api-registration.js
@@ -1,19 +1,24 @@
-const apiRegistration = () => {
+export const userRegistration = (email, password, passwordConfirmation) => {
 
-  const userProfile = {
-    "email": "",
-    "password": "",
-    "password_confirmation": ""
-  }
+  const formData = new FormData()
+    formData.append('email', email);
+    formData.append('password', password);
+    formData.append('password_confirmation', passwordConfirmation)
+
+  // const userProfile = {
+  //   "email": email,
+  //   "password": password,
+  //   "password_confirmation": password_confirmation
+  // }
 
   const requestOptions = {
     method: "POST",
-    body: userProfile,
+    body: formData,
     redirect: 'follow'
   }
 
   fetch("http://206.189.91.54//api/v1/auth/", requestOptions)
-    .then(response => response.text())
+    .then(response => response.json())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
 }
